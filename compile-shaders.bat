275ae235d4761b4a4243350b6a161fd61b652152
Kumodatsu <dragonkumo@gmail.com>
2020-04-01 16:52:55 +0200

    Add Windows script to compile shaders

---
+++ compile-shaders.bat
@@ -0,0 +1,3 @@
+@echo off
+glslc -fshader-stage=vert res/shaders/vertex_shader.glsl -o res/shaders/vertex_shader.spv
+glslc -fshader-stage=frag res/shaders/fragment_shader.glsl -o res/shaders/fragment_shader.spv
