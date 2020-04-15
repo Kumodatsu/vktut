e3982f66b9cc5b96420eb5d3c60832ce0b26911a
Kumodatsu <dragonkumo@gmail.com>
2020-04-15 07:50:33 +0200

    Add Vertex hash

--- projects/VKTut/src/Vertex.hpp
+++ projects/VKTut/src/Vertex.hpp
@@ -1,5 +1,9 @@
 #pragma once
+
 #include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+#undef GLM_ENABLE_EXPERIMENTAL
 
 namespace Kumo {
 
@@ -23,6 +27,25 @@ namespace Kumo {
                 {1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, Color)},
                 {2, 0, VK_FORMAT_R32G32_SFLOAT,    offsetof(Vertex, TextureCoords)}
             }};
+        }
+    };
+
+    inline bool operator == (const Vertex& u, const Vertex& v) {
+        return u.Position      == v.Position
+            && u.Color         == v.Color
+            && u.TextureCoords == v.TextureCoords;
+    }
+
+}
+
+namespace std {
+
+    template <> struct hash<::Kumo::Vertex> {
+        size_t operator () (const ::Kumo::Vertex& v) const {
+            const auto hash_pos = hash<glm::vec3>()(v.Position);
+            const auto hash_col = hash<glm::vec3>()(v.Color) << 1;
+            const auto hash_tex = hash<glm::vec2>()(v.TextureCoords) << 1;
+            return ((hash_pos ^ hash_col) >> 1) ^ hash_tex;
         }
     };
 
